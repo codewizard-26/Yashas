b472b7a9c7d2a650a4b8b4a485e64553ab8d71f6
Harshon20 <singhharsh18433@gmail.com>
2026-06-02 00:43:40 +0530

    added applications schema

---
+++ server/drizzle/schema/applications.schema.js
@@ -0,0 +1,21 @@
+const { jobs } = require("./jobs.schema");
+const { users } = require("./user.scehma");
+const { applicationStatuses } = require("./applicationStatatuses.schema");
+
+const {
+    pgTable,
+    uuid,
+    text,
+    timestamp,
+} = require("drizzle-orm/pg-core");
+
+const applications = pgTable("applications",{
+    id: uuid("id").defaultRandom().primaryKey(),
+    jobId: uuid("job_id").references(() => jobs.id).notNull(),
+    userId: uuid("user_id").references(() => users.id).notNull(),
+    applicationStatusId: uuid("application_status_id").references(() => applicationStatuses.id).notNull(),
+    resumeURL: text("resume_url"),
+    appliedAt: timestamp("applied_at").defaultNow(),
+});
+
+module.exports = { applications };
