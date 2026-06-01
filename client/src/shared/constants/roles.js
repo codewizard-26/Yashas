98729d4476e8a3b7e7447412b6adf6bfdf1d92c2
yashwant <yashwantsinghhustles@gmail.com>
2026-06-01 11:44:32 +0530

    role permission

---
+++ client/src/shared/constants/roles.js
@@ -0,0 +1,9 @@
+export const ROLES = {
+  STUDENT: "student",
+  TEACHER: "teacher",
+  RECRUITER: "recruiter",
+  ORGANIZATION: "organization",
+  ALUMNI: "alumni",
+  COLLEGE_ADMIN: "collegeAdmin",
+  UNIVERSITY_ADMIN: "universityAdmin",
+};
